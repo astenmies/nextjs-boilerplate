686c566af441436276ed0004bcd7619fa99cd5ac
imagine10255 <imagine10255@gmail.com>
2018-01-09 05:31:26 +0800

    修正 package name, 並更改範例位置

--- src/components/organisms/examples/Header/index.js
+++ src/components/organisms/examples/Header/index.js
@@ -6,26 +6,26 @@ import Link from 'next/link';
 
 
 Router.onRouteChangeStart = (url) => {
-    NProgress.start();
+  NProgress.start();
 };
 Router.onRouteChangeComplete = () => NProgress.done();
 Router.onRouteChangeError = () => NProgress.done();
 
 
 export default () => (
-    <div>
-        <Head>
-            {/* Import CSS for nprogress */}
-            <link rel="stylesheet" type="text/css" href="/static/css/nprogress.css"/>
-        </Head>
+  <div>
+    <Head>
+      {/* Import CSS for nprogress */}
+      <link rel="stylesheet" type="text/css" href="/static/css/nprogress.css" />
+    </Head>
 
-        <nav>
-            <Link href="/examples/css-modules"><a>CSS-Modules</a></Link>
-            <br/>
-            <Link href="/examples/i18next"><a>i18Next</a></Link>
-            <br/>
-            <Link href="/examples/page-link"><a>Page-Link</a></Link>
-        </nav>
-    </div>
+    <nav>
+      <Link href="/examples/css-modules"><a>CSS-Modules</a></Link>
+      <br />
+      <Link href="/examples/i18next"><a>i18Next</a></Link>
+      <br />
+      <Link href="/examples/page-link"><a>Page-Link</a></Link>
+    </nav>
+  </div>
 
 );
