97a3dc2aafa38ce3d6260c96735c848309a535c6
Imagine Chiu <chihfan.chiu@igroup.tw>
2018-01-08 16:58:09 +0800

    更改 預設的全域SCSS padding

--- pages/_document.js
+++ pages/_document.js
@@ -1,28 +1,28 @@
-import Document, { Head, Main, NextScript } from 'next/document';
-import { ServerStyleSheet } from 'styled-components';
-import stylesheet from '@styles/main.scss';
+import Document, {Head, Main, NextScript} from 'next/document';
+import {ServerStyleSheet} from 'styled-components';
+import {stylesheet} from '@styles/main.scss';
 
 export default class MyDocument extends Document {
-  static getInitialProps({ renderPage }) {
-    const sheet = new ServerStyleSheet();
-    const page = renderPage(App => props => sheet.collectStyles(<App {...props} />));
-    const styleTags = sheet.getStyleElement();
-    return { ...page, styleTags };
-  }
+    static getInitialProps({renderPage}) {
+        const sheet = new ServerStyleSheet();
+        const page = renderPage(App => props => sheet.collectStyles(<App {...props} />));
+        const styleTags = sheet.getStyleElement();
+        return {...page, styleTags};
+    }
 
-  render() {
-    return (
-      <html>
-        <Head>
-          <style dangerouslySetInnerHTML={{ __html: stylesheet }} />
-          {this.props.styleTags}
-        </Head>
-        <body>
-          {/* _document */}
-          <Main />
-          <NextScript />
-        </body>
-      </html>
-    );
-  }
+    render() {
+        return (
+            <html>
+            <Head>
+                <style dangerouslySetInnerHTML={{__html: stylesheet}}/>
+                {this.props.styleTags}
+            </Head>
+            <body>
+            {/* _document */}
+            <Main/>
+            <NextScript/>
+            </body>
+            </html>
+        );
+    }
 }
